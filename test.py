4acc48d85eff36e2c0dec893613c39969e2152a9
gituser <onlywyn@126.com>
2018-10-24 03:00:17 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,35 @@
+#li = [1,4,'wyn']
+#print(li)
+#li.append('fuck')
+#print(li[3])
+#print(li)
+#li.insert(3,[33,44,55])
+#print(li)
+#li.pop(2)
+#print(li)
+#while 1:
+#	username = input(">>>>>")
+#	if username.strip().upper() == 'Q':
+#		break
+#	else:
+#		li.append(username)
+#
+#print(li)
+
+#li2 = [1,2,3]
+#li.extend(li2)
+#print(li)
+#li.extend("中国人")
+#print(li)
+#print(li.count)
+print('---------------')
+li = ['nihao','中国人','日本','23']
+print(li[1][1])
+print('---------------')
+s1='+++'.join(li)
+print(s1)
+print('---------------')
+
+for i in range(0,10,-1):
+	print(i)
+print('---------------')
